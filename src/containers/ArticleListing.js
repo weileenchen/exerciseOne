28e7ddd020a9f43a4e3987044c4e9c196c045614
weileenchen <wc1627@nyu.edu>
2021-09-16 09:51:18 -0400

    CSS

--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ArticleCard from "./../components/ArticleCard";
+import Data from "../components/data.json";
 
 function ArticleListing() {
+  const articlesTitle = `Articles`;
   return (
     <div className="PageWrapper">
-      <h1 className="HeaderOneStyle">Articles</h1>
+      <h1 className="HeaderOneStyle">{articlesTitle}</h1>
+      {Data.map((article, key) => (
+        <ArticleCard article={article} key={key} />
+      ))}
     </div>
   );
 }
