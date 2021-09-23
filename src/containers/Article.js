a01f893d8ea2bd0d2119e0be279f4797bc949e46
weileenchen <wc1627@nyu.edu>
2021-09-22 23:25:42 -0400

    Article page

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Data from "../components/data.json";
 
 function Article() {
-  return <div>Single Article</div>;
+  const [article, setArticle] = useState({});
+  let { id } = useParams();
+
+  useEffect(() => {
+    const dataToSet = Data.find((item) => item.id === id);
+    setArticle(dataToSet);
+  }, [id]);
+
+  return (
+    <main>
+      <section className="ArticleHeader">
+        <div className="ArticleHeaderText">
+          <h1 className="HeaderOneStyle">{article.title}</h1>
+          <p className="ArticleCardDate">{article.publishedDate}</p>
+          <p className="ArticleHeaderBlurb">{article.blurb}</p>
+        </div>
+      </section>
+      <section className="ArticleText">
+        {article.articleText && article.articleText.map((text, i) => {
+          return <p key={i}>{text.data}</p>
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default Article;
